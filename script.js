bb46d884201bea75681271abda0a45a2663e205b
Raffaele Balsamo <raffob003@gmail.com>
2023-03-06 12:46:59 +0100

    canvas created

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = docoument.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png'
